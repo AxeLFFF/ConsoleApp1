6e423b300a5236a5a4618dcaf7c415a974246f4f
AxeLFFF <workbox91@gmail.com>
2018-04-17 21:34:35 +0600

    New commit again

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello .NET Core!");
+            Console.WriteLine("Hello .NET Core again!");
         }
     }
 }
