683d21342b72579cfb981c1e19bf9d2b54bb500d
AxeLFFF <workbox91@gmail.com>
2018-04-18 21:13:49 +0600

    Test Google Speech service

---
+++ ConsoleApp1/TextToSpeechProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.Cloud.Speech.V1;
+
+namespace ConsoleApp1
+{
+    public static class TextToSpeechProvider
+    {
+        public static void Audio2Text(string filepath)
+        {
+            var speechClient = SpeechClient.Create();
+            var response = speechClient.Recognize(new RecognitionConfig()
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                SampleRateHertz = 16000,
+                LanguageCode = "ru"
+            }, RecognitionAudio.FromFile(filepath));
+            foreach(var result in response.Results)
+            {
+                foreach(var alternative in result.Alternatives)
+                {
+                    Console.WriteLine(alternative.Transcript);
+                }
+            }
+        }
+    }
+}
